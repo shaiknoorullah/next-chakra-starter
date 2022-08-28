3e65ba5893f127489734e0e9dcdea3d90ddd91e2
Shaik Noorullah Shareef <shaiknooru247@gmail.com>
2022-08-28 21:48:14 +0530

    initial

--- components/navbar.js
+++ components/navbar.js
@@ -1,40 +1,38 @@
-import Logo from './logoNext'
+// import Logo from './logoNext'
 import NextLink from 'next/link'
 import {
-  Container,
   Box,
   Link,
   Stack,
-  Heading,
   Flex,
+  Image,
   Menu,
   MenuItem,
   MenuList,
   MenuButton,
   IconButton,
-  useColorModeValue
+  useColorModeValue,
+  Button
 } from '@chakra-ui/react'
 import { Menu as HamburgerIcon } from '@mui/icons-material'
-import ToggleColorModeButton from './toggleColorMode'
 
-const LinkItem = ({ href, path, target, children, ...props }) => {
+import TrueRoas from './trueRoas.svg'
+
+const LinkItem = ( { href, path, variant, target, children, ...props } ) => {
   const active = path === href
-  const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
-  const activeColor = useColorModeValue('#309794', '#fbd38d')
+  const inactiveColor = useColorModeValue( 'gray200', 'whiteAlpha.900' )
+  const activeColor = useColorModeValue( '#309794', '#fbd38d' )
   return (
-    <NextLink href={href} passHref scroll={false}>
-      <Stack display={'flex'} flexDirection={'column'} m={0} p={0}>
+    <NextLink href={ href } passHref scroll={ false }>
+      <Stack display={ 'flex' } flexDirection={ 'row' } m={ 0 } p={ 0 }>
         <Link
-          pt={1}
-          px={2}
-          mb={-1}
-          className={'hover-underline-animation'}
-          borderRadius={'lg'}
-          color={active ? activeColor : inactiveColor}
-          target={target}
-          {...props}
+          p={ "10px" }
+          variant={ variant }
+          color={ active ? activeColor : inactiveColor }
+          target={ target }
+          { ...props }
         >
-          {children}
+          { children }
         </Link>
       </Stack>
     </NextLink>
@@ -45,91 +43,125 @@ const Navbar = props => {
   const { path } = props
 
   return (
-    <Box
-      position="fixed"
-      as="nav"
-      w="100%"
-      bg={useColorModeValue('#ffffff40', '#20202380')}
-      css={{ backdropFilter: 'blur(10px)' }}
-      zIndex={1}
-      {...props}
-    >
-      <Container
-        display="flex"
-        p={2}
-        maxW="1200px"
-        wrap="wrap"
-        align="center"
-        justify="space-between"
-      >
-        <Flex align="center">
-          <Heading as="h1" size="lg" letterSpacing={'tighter'}>
-            <Logo />
-          </Heading>
-        </Flex>
 
-        <Stack
-          direction={{ base: 'column', md: 'row' }}
-          display={{ base: 'none', md: 'flex' }}
-          width={{ base: 'full', md: 'auto' }}
-          align="center"
-          justifyContent="center"
-          flexGrow={1}
-          fontWeight="semibold"
-          mt={{ base: 4, md: 0 }}
-          ml="6"
-        >
-          <LinkItem
-            ml="-20"
-            href="/documentation"
-            path={path}
-            style={{ textDecoration: 'none' }}
-          >
-            Documentation
+    <Box w={ 'full' } position={ 'fixed' }>
+      <Stack bgColor={ 'blackAlpha.400' } direction={ 'row' }
+        // spacing={ { xl: 60, lg: 50, md: 30 } }
+        maxW={ { xl: '1900px', lg: '1400px', md: '1200px', sm: '650px', xs: '300px' } }
+        spacing={ {} }
+        alignItems={ 'space-between' }
+        justifyContent={ 'center' }>
+        <Box p={ 2 } position={ 'relative' } align={ 'left' } bgColor={ 'telegram.500' }>
+          <TrueRoas />
+        </Box>
+        <Stack display={ { lg: "flex", md: 'none', sm: 'none', base: 'none' } } bgColor={ 'telegram.500' } direction={ { lg: 'row', md: 'row', sm: 'row', xs: 'column' } } justifyContent={ 'center' } spacing={ 10 } alignItems={ 'center' } >
+          <LinkItem href="/" path={ path }>
+            Why TrueRoas?
           </LinkItem>
-          <LinkItem
-            mr="20"
-            href="https://github.com/shaiknoorullah"
-            path={path}
-            style={{ textDecoration: 'none' }}
-          >
-            Github
+          <LinkItem href="/" path={ path }>
+            Why TrueRoas?
+          </LinkItem>
+          <LinkItem href="/" path={ path }>
+            Why TrueRoas?
+          </LinkItem>
+          <LinkItem href="/" path={ path }>
+            Why TrueRoas?
           </LinkItem>
         </Stack>
-
-        <Box flex={1} align="right" maxW={{ base: undefined, md: '70px' }}>
-          <ToggleColorModeButton />
-
-          <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
-            <Menu isLazy id="navbar-menu">
-              <MenuButton
-                as={IconButton}
-                icon={<HamburgerIcon />}
-                variant="outline"
-                aria-label="Options"
-              />
-              <MenuList>
-                <NextLink href="/" passHref>
-                  <MenuItem as={Link} style={{ textDecoration: 'none' }}>
-                    About
-                  </MenuItem>
-                </NextLink>
-                <NextLink href="/works" passHref>
-                  <MenuItem as={Link} style={{ textDecoration: 'none' }}>
-                    Works
-                  </MenuItem>
-                </NextLink>
-                <NextLink href="/coming-soon" passHref>
-                  <MenuItem as={Link} style={{ textDecoration: 'none' }}>
-                    Posts
-                  </MenuItem>
-                </NextLink>
-              </MenuList>
-            </Menu>
-          </Box>
+        <Box p={ 2 }>
+          <Stack direction={ 'row' } spacing={ 10 } display={ { lg: 'flex', md: 'flex', base: 'none' } } justifyContent={ 'center' } alignItems={ 'center' }>
+            <Button variant={ 'signup' } color={ "#70728B" } w={ "auto" } bgColor={ 'transparent' } size={ "lg" } >Login</Button>
+            <Button variant={ 'signup' } size={ "lg" } >Signup For Free</Button>
+          </Stack>
         </Box>
-      </Container>
+      </Stack>
     </Box>
+
+    // <Box width={ { lg: 'full', md: "md", base: "xs" } } display={ "flex" } py={ 2 } px={ 2 } justifyContent={ "center" } alignItems={ "center" } position={ "fixed" }>
+
+    //   <Stack justifyContent={ "space-between" } w={ { lg: "1700px", md: "1300px", base: "700px" } } alignItems={ "center" } spacing={ { lg: 40, md: 20, base: 10 } } direction={ "row" } >
+
+    //     <Flex align="center" justifyContent={ "center" } >
+    //       <Image src={ "/trueRoas.svg" } alt={ "logo" } />
+    //     </Flex>
+    //     <Stack direction={ "row" } justifyContent={ "center" } alignItems={ "center" } spacing={ 40 }>
+
+    //       <Stack
+    //         direction={ { base: 'column', md: 'row' } }
+    //         display={ { base: 'none', md: 'flex' } }
+    //         width={ { base: '800px', md: 'auto' } }
+    //         alignItems="center"
+    //         justifyContent="center"
+    //         flexGrow={ 1 }
+    //         fontWeight="semibold"
+    //         spacing={ 10 }
+    //       >
+    //         <LinkItem
+    //           href="/"
+    //           path={ path }
+
+    //           style={ { textDecoration: 'none' } }
+    //         >
+    //           Why TrueROAS?
+    //         </LinkItem>
+    //         <LinkItem
+    //           href="/features"
+    //           path={ path }
+    //           style={ { textDecoration: 'none' } }
+    //         >
+    //           Features
+    //         </LinkItem>
+    //         <LinkItem
+    //           href="/solutions"
+    //           path={ path }
+    //           style={ { textDecoration: 'none' } }
+    //         >
+    //           Solutions
+    //         </LinkItem>
+    //         <LinkItem
+    //           href="pricing"
+    //           path={ path }
+    //           style={ { textDecoration: 'none' } }
+    //         >
+    //           Pricing
+    //         </LinkItem>
+    //       </Stack>
+
+    //     </Stack>
+    //     <Stack justifyContent={ "center" } alignItems={ "center" } direction={ { lg: 'row', base: "column" } } spacing={ 5.4 }>
+    //       <Box ml={ 2 } display={ { base: 'inline-block', md: 'none' } }>
+    //         <Menu isLazy id="navbar-menu">
+    //           <MenuButton
+    //             as={ IconButton }
+    //             icon={ <HamburgerIcon /> }
+    //             variant="outline"
+    //             aria-label="Options"
+    //           />
+    //           <MenuList>
+    //             <NextLink href="/" passHref>
+    //               <MenuItem as={ Link } style={ { textDecoration: 'none' } } >About</MenuItem>
+    //             </NextLink>
+    //             <NextLink href="/works" passHref>
+    //               <MenuItem as={ Link } style={ { textDecoration: 'none' } } >Works</MenuItem>
+    //             </NextLink>
+    //             <NextLink href="/coming-soon" passHref>
+    //               <MenuItem as={ Link } style={ { textDecoration: 'none' } } >Posts</MenuItem>
+    //             </NextLink>
+    //             {/* <MenuItem
+    //               as={Link}
+    //               href="https://github.com/craftzdog/craftzdog-homepage"
+    //             >
+    //               View Source
+    //             </MenuItem> */}
+    //           </MenuList>
+    //         </Menu>
+    //       </Box>
+    //       {/* <NextLink variant={"navButton"} href={""} */ }
+    //       <Button variant={ 'signup' } color={ "#70728B" } w={ "auto" } bgColor={ 'transparent' } size={ "lg" } >Login</Button>
+    //       <Button variant={ 'signup' } size={ "lg" } >Signup for Free</Button>
+    //     </Stack>
+    //   </Stack>
+    // </Box>
   )
 }
 
